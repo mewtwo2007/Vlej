6f7d059239a4bb80e88adad5508e968ae88bd9b2
mewtwo2007 <zshep31415@outlook.com>
2018-07-29 13:17:18 -0700

    fixing the bug in linux

--- install_linux.sh
+++ install_linux.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "The Program will be installe in /vlej/1.0"
+echo "The Program will be installed in /vlej/1.0"
 mkdir /vlej
 mkdir /vlej/1.0
 cd Source
 jar cvfm vlej.jar manifest.txt *.class
 cp vlej.jar /vlej/1.0
 cd /bin
-cat > vlej.sh << EOF1
+cat > vlej << EOF1
 #!/bin/bash
 java -jar /vlej/1.0/vlej.jar $1
 EOF1
