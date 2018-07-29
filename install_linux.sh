788bccc0c0b5ca8c1c1e5ca6a8565cf70149ab83
mewtwo2007 <zshep31415@outlook.com>
2018-07-28 20:14:01 -0700

    install script for linux

--- install_linux.sh
+++ install_linux.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+echo "The Program will be installe in /vlej/1.0"
+mkdir /vlej
+mkdir /vlej/1.0
+jar cvfm vlej.jar manifest.txt *.class
+cp vlej.jar /vlej/1.0
+cd /bin
+cat > vlej.sh << EOF1
+#!/bin/bash
+java -jar /vlej/1.0/vlej.jar $1
+EOF1
